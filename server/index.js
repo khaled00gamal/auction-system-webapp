99464cef542b2ea452d43eaff41497743a28bfa5
Khaled gamal <khaled2000s.kg@gmail.com>
2023-03-10 01:17:25 +0200

    securing serviceAccount as environment variable

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
