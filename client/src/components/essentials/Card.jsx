ce8b82d7c38fa5eed4113f24f2035bee54b76f67
Khaled Ashraf <khaledashrafelmaleh@gmail.com>
2023-03-14 15:50:58 +0200

    Added Carousel component with styling

--- client/src/components/essentials/Card.jsx
+++ client/src/components/essentials/Card.jsx
@@ -3,16 +3,16 @@ import Button from "./Button";
 import "../styles/Card.css";
 import { propTypes } from "react-bootstrap/esm/Image";
 
-function Card() {
+function Card(props) {
     return (
         <div className="card-wrapper">
             <div className="image-and-caption-wrapper">
                 <div className="image-container">
-                    <img className="image" src={props.itemImageLink} />
+                    <img className="image" src={props.itemImageLink} alt="" />
                 </div>
                 <p>{props.itemName}</p>
             </div>
-            <Button text="khaled" width="276px" size="medium" />
+            <Button text="View" width="276px" size="medium" link="/"/>
         </div>
     );
 
---
+++ client/src/components/essentials/Carousel.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Card from "../essentials/Card";
+import "../styles/Carousel.css"
+
+function Carousel(props) {
+    return (
+        <div className="carousel-wrapper">
+            <div className="title-and-view-all-wrapper">
+                <div className="title"><h3>Title</h3></div>
+                <div className="view-all"><p>View All</p></div>
+            </div>
+            <div className="cards-wrapper">
+                <Card itemName="item" itemImageLink="https://kelleherstampassets.s3.us-east-2.amazonaws.com/Sales/655/387876.jpg"/>
+                <Card itemName="item" itemImageLink="https://kelleherstampassets.s3.us-east-2.amazonaws.com/Sales/655/387876.jpg"/>
+                <Card itemName="item" itemImageLink="https://kelleherstampassets.s3.us-east-2.amazonaws.com/Sales/655/387876.jpg"/>
+            </div>
+        </div>
+    );
+}
+
+export default Carousel;
