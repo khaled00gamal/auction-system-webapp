6c6c7293c449b1982819535ba3c7d665b15c6318
Khaled Ashraf <khaledashrafelmaleh@gmail.com>
2023-03-16 17:46:00 +0200

    Updated button component
    added a naming convention for using it, and added a text style

--- client/src/components/essentials/Button.jsx
+++ client/src/components/essentials/Button.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "../styles/Button.css";
 
+/*
+the way a button style is selected is through this naming convention:
+button-size-style
+
+size and style are necessary conditions when using this component
+*/
+
 function Button(props) {
-  let variant = "button-" + props.size;
+  let variant = "button";
+
+  if (props.size) {
+    variant = variant + "-" + props.size;
+  }
+
+  if (props.style) {
+    variant = variant + "-" + props.style;
+  }
+
+  //let variant = "button-" + props.size + "-" + props.style;
   return (
     <a className={variant} href={props.link} style={{ minWidth: props.width }}>
       {props.text}
