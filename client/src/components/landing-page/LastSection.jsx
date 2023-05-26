4a7c7a0aa489304104ab062f4c714e788ce95ab6
Khaled Ashraf <khaledashrafelmaleh@gmail.com>
2023-05-27 02:10:32 +0300

    Update LastSection.jsx

--- client/src/components/landing-page/LastSection.jsx
+++ client/src/components/landing-page/LastSection.jsx
@@ -1,19 +1,26 @@
 import '../styles/LastSection.css';
 import { Link } from 'react-router-dom';
+import Button from '../essentials/Button';
 
 function LastSection() {
   return (
     <div id='last-section'>
-      <div className='row'>
+      <div className='row-last-section'>
         <div className='last-section-all'>
-          <p className='bold'>With an account you can:</p>
+          <h1>With an account you can:</h1>
           <ul className='last-section-ul'>
             <li className='bold-li'>TRACK YOUR ACTIVE BIDS</li>
             <li className='bold-li'>SAVE TO FAVORITES</li>
             <li className='bold-li'>SELL GOODS</li>
           </ul>
           <Link to='signin'>
-            <button className='last-section-button'>Create account</button>
+            <Button
+              text='Create Account'
+              size='extra-large'
+              style='regular'
+              color='pink'
+            />
+            {/* <button className='last-section-button'>Create account</button> */}
           </Link>
         </div>
         <div className='last-section-img'>
