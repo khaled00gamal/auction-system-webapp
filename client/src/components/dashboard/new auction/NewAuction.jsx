542e7ca2b61066d6de5129815c05584460bb41a4
Haazem66 <61153924+Haazem66@users.noreply.github.com>
2023-06-06 01:08:56 +0300

    + styling

--- client/src/components/dashboard/new auction/NewAuction.jsx
+++ client/src/components/dashboard/new auction/NewAuction.jsx
@@ -231,11 +231,7 @@ function NewAuction() {
 
 
             <div className='item-info-wrapper'>
-
-
-
-
-
+            
               <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
                 <div className="mb-4 flex flex-col gap-6">
                   <div class="w-72">
