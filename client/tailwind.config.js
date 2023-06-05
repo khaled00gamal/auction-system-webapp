90f9b3f7a11f65c1f03d5078c838790bf593967d
Haazem66 <61153924+Haazem66@users.noreply.github.com>
2023-06-06 01:28:24 +0300

    more and more

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require("@material-tailwind/html/utils/withMT");
+module.exports = withMT({
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
@@ -7,5 +8,5 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+})
 
