4791cab44c9db546cd301782597bd91b71b76248
Amr Ayman <miroamr78@gmail.com>
2023-07-04 16:32:14 +0300

    Update contract to reflect new approach

--- contracts/contracts/SealedBidAuctionManager.sol
+++ contracts/contracts/SealedBidAuctionManager.sol
@@ -1,264 +1,332 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-// NOTE: All currency is wei
-
-// TODO: compute securityDeposit = minimumPrice * 0.02
+// NOTE: all currency is wei
 
 contract SealedBidAuctionManager {
-    /**
-     * types
-     */
-
     struct UserSetAuctionInfo {
-        address payable seller;
-        // auction rules
-        uint256 securityDeposit; // FIXME
-        uint256 minimumPrice;
-        // dates
-        uint256 biddingEndDate;
-        uint256 confirmationEndDate;
+        // administrative
+        address payable owner;
+        string params;
+        string owner_pkey;
         // item
         string itemName;
         string itemDesc;
         string itemPicture;
+        // transactional
+        uint256 minimumPrice;
+        uint256 securityDeposit;
+        // dates
+        uint256 biddingEndDate;
+        uint256 revealingEndDate;
+        uint256 sortingEndDate;
+        uint256 verificationEndDate;
     }
 
-    enum AuctionState {
-        Active,
-        Failure,
-        Success,
-        InConfirmation
+    struct Bid {
+        // to bid
+        bytes32 c;
+        bytes32 d;
+        // to reveal
+        string z;
     }
 
-    struct PublicAuctionInfo {
-        uint256 auctionId;
-        UserSetAuctionInfo userSet;
-        AuctionState state;
+    struct AuctionWinner {
+        string y;
+        address bidder;
     }
 
     struct Auction {
-        PublicAuctionInfo info;
-        // protocol; redundant, but convenient
-        uint32 highestBid;
-        address payable highestBidder; // keys into bids
-        // participants
-        mapping(address => uint32) bids;
-
-        // bids maps bidder to their pedersen-commited bid.
+        uint256 id;
+        UserSetAuctionInfo info;
+        mapping(address => Bid) bids;
+        address[] bidders;
+        AuctionState state;
+        AuctionWinner winner;
     }
 
-    /**
-     * state variables
-     */
+    enum AuctionState {
+        BiddingPhase,
+        RevealingPhase,
+        SortingPhase,
+        VerificationPhase,
+        Success,
+        Failure
+    }
 
     Auction[] private auctions;
 
-    /**
-     * events
-     */
+    // events for logging and subscription
 
-    // for logging
     event NewAuction(uint256 auctionId);
-    event NewBid(address by);
+    event NewBid(uint256 auctionId, address by);
+    event NewReveal(uint256 auctionId, address by);
+    event BidderRemoved(uint256, address who);
+    event WinnerDecided(uint256 auctionId);
+    event ObjectionSuccessful(uint256 auctionId, address by);
+    event AuctionEnds(uint256 auctionId, bool success);
 
-    // for subscription
-    event AuctionEnds(bool success);
-    event AuctionWasWon(address winner);
+    // modifiers for modularity
 
-    /**
-     * modifiers
-     */
-
-    modifier newBidder(uint256 auctionId) {
-        if (
-            auctions[auctionId].info.userSet.seller == msg.sender ||
-            auctions[auctionId].bids[msg.sender] != 0
-        ) revert("Sender is either the seller or has already placed a bid!");
+    modifier SenderDidNotPlaceABid(uint256 auctionId) {
+        if (auctions[auctionId].bids[msg.sender].c.length != 0)
+            revert("Sender has already placed a bid!");
         _;
     }
 
-    modifier existingBidder(uint256 auctionId) {
-        if (auctions[auctionId].bids[msg.sender] == 0)
+    modifier SenderPlacedABid(uint256 auctionId) {
+        if (auctions[auctionId].bids[msg.sender].c.length == 0)
             revert("Sender has not placed a bid!");
         _;
     }
 
-    modifier onlySeller(uint256 auctionId) {
-        if (msg.sender != auctions[auctionId].info.userSet.seller)
-            revert("Sender is not the seller!");
+    modifier SenderIsOwner(uint256 auctionId) {
+        if (msg.sender != auctions[auctionId].info.owner)
+            revert("Sender is not the owner!");
         _;
     }
 
-    modifier validAuctionId(uint256 auctionId) {
-        if (auctionId >= auctions.length) revert("Invalid auction ID!");
+    modifier SenderIsNotOwner(uint256 auctionId) {
+        if (msg.sender != auctions[auctionId].info.owner)
+            revert("Sender is the owner!");
         _;
     }
 
-    modifier setAuctionState(uint256 auctionId) {
-        if (block.timestamp < auctions[auctionId].info.userSet.biddingEndDate)
-            auctions[auctionId].info.state = AuctionState.Active;
-        else if (!minimumPriceRequirementMet(auctionId))
-            auctions[auctionId].info.state = AuctionState.Failure;
-        else if (
-            block.timestamp <
-            auctions[auctionId].info.userSet.confirmationEndDate
-        ) auctions[auctionId].info.state = AuctionState.InConfirmation;
-        else if (auctions[auctionId].info.state != AuctionState.Success)
-            auctions[auctionId].info.state = AuctionState.Failure;
-        else auctions[auctionId].info.state = AuctionState.Success;
+    modifier AuctionIDIsValid(uint256 auctionId) {
+        if (auctionId >= auctions.length) revert("Auction ID is not valid!");
         _;
     }
 
-    // make sure auction is active or in confirmation
-    modifier checkAuctionStateIs(uint256 auctionId, AuctionState state) {
-        if (state != auctions[auctionId].info.state) {
-            if (state == AuctionState.Active)
-                revert("Bidding period has ended!");
-            if (state == AuctionState.InConfirmation)
-                revert("Not in confirmation period!");
+    modifier CheckAuctionState(uint256 auctionId, AuctionState state) {
+        // set it
+        if (block.timestamp < auctions[auctionId].info.biddingEndDate)
+            auctions[auctionId].state = AuctionState.BiddingPhase;
+        else if (block.timestamp < auctions[auctionId].info.revealingEndDate)
+            auctions[auctionId].state = AuctionState.RevealingPhase;
+        else if (block.timestamp < auctions[auctionId].info.sortingEndDate)
+            auctions[auctionId].state = AuctionState.SortingPhase;
+        else if (block.timestamp < auctions[auctionId].info.verificationEndDate)
+            auctions[auctionId].state = AuctionState.VerificationPhase;
+        // else, state == AuctionState.Success || state == AuctionState.Failure
+
+        // check it
+        if (state != auctions[auctionId].state) {
+            revert("Inappropriate operation!");
         }
         _;
     }
 
-    // make sure auction is not active
-    modifier checkAuctionStateIsNot(uint256 auctionId, AuctionState state) {
-        if (state == auctions[auctionId].info.state) {
-            if (state == AuctionState.Active)
-                revert("Auction is still active!");
-        }
+    modifier ValueEquals(uint v) {
+        if (msg.value != v) revert("Sent value is not right!");
         _;
     }
 
-    /**
-     * private functions
-     */
+    // private functions
 
-    function checkThatCommitmentIsPositive(
-        string memory commitment
-    ) internal pure returns (bool) {}
+    function _stringToBytes32(
+        string memory source
+    ) private pure returns (bytes32 result) {
+        bytes memory b = bytes(source);
+        if (b.length == 0) return 0;
 
-    function minimumPriceRequirementMet(
-        uint256 auctionId
-    ) internal pure returns (bool) {
-        return true; // FIXME
+        assembly {
+            result := mload(add(source, 32))
+        }
     }
 
-    /**
-     * external functions
-     */
+    function _removeBidder(uint256 auctionId, uint index) internal {
+        require(index < auctions[auctionId].bidders.length);
 
-    // functions to be called at any time
+        auctions[auctionId].bidders[index] = auctions[auctionId].bidders[
+            auctions[auctionId].bidders.length - 1
+        ];
+        auctions[auctionId].bidders.pop();
+    }
+
+    // external functions to be called at any time
 
     function getNumberOfRegisteredAuctions() external view returns (uint256) {
         return auctions.length;
     }
 
     function createAuction(
-        UserSetAuctionInfo memory auctionInfo
-    ) external returns (uint256 auctionId) {
-        // create an auctionId, then add to table
-        auctionId = auctions.length;
+        UserSetAuctionInfo calldata info
+    ) external payable ValueEquals(info.securityDeposit) returns (uint256) {
+        // validation of auction parameters
+        if (info.minimumPrice == 0)
+            revert("Minimum price must be a positive number");
+
+        if (
+            info.securityDeposit > info.minimumPrice ||
+            info.securityDeposit == 0
+        )
+            revert(
+                "The relation (0 < Security Deposit < Minimum Price) is not satisfied."
+            );
+
+        if (
+            !(info.verificationEndDate > info.sortingEndDate &&
+                info.sortingEndDate > info.revealingEndDate &&
+                info.revealingEndDate > info.biddingEndDate)
+        ) revert("Dates are not in the right order.");
+
+        // valid? add it
         Auction storage auction = auctions.push();
-        auction.info = PublicAuctionInfo({
-            auctionId: auctionId,
-            userSet: auctionInfo,
-            state: AuctionState.Active
-        });
-        emit NewAuction(auctionId);
+
+        auction.id = auctions.length;
+        auction.info = info;
+        auction.state = AuctionState.BiddingPhase;
+        auction.winner = AuctionWinner({y: "", bidder: address(0)});
+
+        emit NewAuction(auction.id);
+        return auction.id;
     }
 
-    function getAuctionData(
+    function getAuctionsByState(
+        AuctionState state
+    ) external view returns (UserSetAuctionInfo[] memory) {
+        uint256 count = 0;
+        for (uint256 i = 0; i < auctions.length; i++) {
+            if (auctions[i].state == state) {
+                count++;
+            }
+        }
+
+        UserSetAuctionInfo[] memory data = new UserSetAuctionInfo[](count);
+        uint256 j = 0;
+        for (uint256 i = 0; i < auctions.length; i++) {
+            if (auctions[i].state == state) {
+                data[j] = auctions[i].info;
+                j++;
+            }
+        }
+
+        return data;
+    }
+
+    function getAuctionInfo(
         uint256 auctionId
     )
         external
         view
-        validAuctionId(auctionId)
-        returns (PublicAuctionInfo memory)
+        AuctionIDIsValid(auctionId)
+        returns (UserSetAuctionInfo memory)
     {
         return auctions[auctionId].info;
     }
 
-    function getActiveAuctions()
-        external
-        view
-        returns (PublicAuctionInfo[] memory)
-    {
-        uint256 activeCount = 0;
-        for (uint256 i = 0; i < auctions.length; i++) {
-            if (auctions[i].info.state == AuctionState.Active) {
-                activeCount++;
-            }
-        }
-
-        PublicAuctionInfo[] memory activeAuctions = new PublicAuctionInfo[](
-            activeCount
-        );
-        uint256 currentIndex = 0;
-        for (uint256 i = 0; i < auctions.length; i++) {
-            if (auctions[i].info.state == AuctionState.Active) {
-                activeAuctions[currentIndex] = auctions[i].info;
-                currentIndex++;
-            }
-        }
-
-        return activeAuctions;
-    }
-
-    // functions to be called during bidding period
+    // functions to be called throughout the auction process
 
     function placeBid(
         uint256 auctionId,
-        uint32 bid
+        string calldata c,
+        string calldata d
     )
         external
         payable
-        validAuctionId(auctionId)
-        newBidder(auctionId)
-        setAuctionState(auctionId)
-        checkAuctionStateIs(auctionId, AuctionState.Active)
+        AuctionIDIsValid(auctionId)
+        SenderIsNotOwner(auctionId)
+        SenderDidNotPlaceABid(auctionId)
+        CheckAuctionState(auctionId, AuctionState.BiddingPhase)
+        ValueEquals(auctions[auctionId].info.securityDeposit)
     {
-        // transfer security deposit to contract
-        if (msg.value != auctions[auctionId].info.userSet.securityDeposit)
-            revert("Sent deposit is not equal to required deposit!");
-        // hash bid by address (FIXME: ??)
-        // bytes32 b = hash(bid);
-        auctions[auctionId].bids[msg.sender] = bid;
-        emit NewBid(msg.sender);
+        auctions[auctionId].bids[msg.sender] = Bid({
+            c: _stringToBytes32(c),
+            d: _stringToBytes32(d),
+            z: ""
+        });
+        auctions[auctionId].bidders[auctions[auctionId].bidders.length] = msg
+            .sender;
+        emit NewBid(auctionId, msg.sender);
     }
 
-    // functions to be called after auction ends
+    function revealBid(
+        uint256 auctionId,
+        string calldata z
+    )
+        external
+        AuctionIDIsValid(auctionId)
+        SenderPlacedABid(auctionId)
+        CheckAuctionState(auctionId, AuctionState.RevealingPhase)
+    {
+        // verify commitment
+        if (sha256(bytes(z)) != auctions[auctionId].bids[msg.sender].d)
+            revert("Hash is invalid!");
 
-    function returnDeposit(
+        auctions[auctionId].bids[msg.sender].z = z;
+        emit NewReveal(auctionId, msg.sender);
+    }
+
+    function getReveals(
         uint256 auctionId
     )
         external
-        validAuctionId(auctionId)
-        existingBidder(auctionId)
-        setAuctionState(auctionId)
-        checkAuctionStateIsNot(auctionId, AuctionState.Active)
+        AuctionIDIsValid(auctionId)
+        SenderIsOwner(auctionId)
+        CheckAuctionState(auctionId, AuctionState.SortingPhase)
+        returns (string[] memory)
     {
-        // no re-entrancy attacks!
-        auctions[auctionId].bids[msg.sender] = 0;
-        payable(msg.sender).transfer(
-            auctions[auctionId].info.userSet.securityDeposit
-        );
+        uint256 count = 0;
+        for (uint256 i = 0; i < auctions[auctionId].bidders.length; i++) {
+            address bidder = auctions[auctionId].bidders[i];
+            if (bytes(auctions[auctionId].bids[bidder].z).length == 0) {
+                // remove them, no refunds
+                _removeBidder(auctionId, i);
+                emit BidderRemoved(auctionId, bidder);
+            } else {
+                count++;
+            }
+        }
+
+        string[] memory zs = new string[](count);
+        for (uint256 i = 0; i < auctions[auctionId].bidders.length; i++) {
+            zs[i] = auctions[auctionId].bids[auctions[auctionId].bidders[i]].z;
+        }
+
+        return zs;
     }
 
-    // TODO two-sign
-    function transferWinningBidToOwner(
-        uint256 auctionId
+    function declareWinner(
+        uint256 auctionId,
+        AuctionWinner calldata winner
     )
         external
-        validAuctionId(auctionId)
-        onlySeller(auctionId)
-        setAuctionState(auctionId)
-        checkAuctionStateIs(auctionId, AuctionState.InConfirmation)
+        AuctionIDIsValid(auctionId)
+        SenderIsOwner(auctionId)
+        CheckAuctionState(auctionId, AuctionState.SortingPhase)
     {
-        auctions[auctionId].info.state = AuctionState.Success;
-        // FIXME: "unhash" the bid
-        auctions[auctionId].highestBidder.transfer(
-            auctions[auctionId].highestBid
-        );
+        // verify commitment
+        if (
+            sha256(bytes(winner.y)) != auctions[auctionId].bids[winner.bidder].c
+        ) revert("Hash is invalid!");
+
+        auctions[auctionId].winner = winner;
+        emit WinnerDecided(auctionId);
+    }
+
+    function getWinner(
+        uint256 auctionId
+    ) external view AuctionIDIsValid(auctionId) returns (AuctionWinner memory) {
+        return auctions[auctionId].winner;
+    }
+
+    function objectToWinner(
+        uint256 auctionId,
+        string calldata y
+    )
+        external
+        AuctionIDIsValid(auctionId)
+        SenderPlacedABid(auctionId)
+        CheckAuctionState(auctionId, AuctionState.VerificationPhase)
+    {
+        // verify commitment
+        if (sha256(bytes(y)) != auctions[auctionId].bids[msg.sender].c)
+            revert("Hash is invalid!");
+
+        // verify that it is bigger
+        revert("Not implemented"); // FIXME
+
+        // auctions[auctionId].winner = winner;
+        // emit WinnerDecided(auctionId, winner.bidder);
     }
 }
